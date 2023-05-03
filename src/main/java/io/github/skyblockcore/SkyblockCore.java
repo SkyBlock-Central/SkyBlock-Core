7610333014b9a54be0ce06a514000dee7dfd423c
Kyllian <kylleke@telenet.be>
2023-05-03 15:20:19 +0200

    skyblock location
    gets the player's custom Skyblock Location from the sidebar and stores it

--- src/main/java/io/github/skyblockcore/SkyblockCore.java
+++ src/main/java/io/github/skyblockcore/SkyblockCore.java
@@ -3,17 +3,22 @@ package io.github.skyblockcore;
 import io.github.skyblockcore.event.JoinSkyblockCallback;
 import io.github.skyblockcore.event.LeaveSkyblockCallback;
 import net.fabricmc.api.ClientModInitializer;
-import net.minecraft.client.MinecraftClient;
-import net.minecraft.text.Text;
 import net.minecraft.util.ActionResult;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+
+import java.util.Objects;
 
 public class SkyblockCore implements ClientModInitializer {
     public static final String ModID = "skyblockcore";
 
     private static boolean ON_SKYBLOCK = false;
     public static boolean isOnSkyblock() { return ON_SKYBLOCK; }
+    public static String getLocation() { return LOCATION; }
+    public static void setLocation(String newLocation) {
+        if (!Objects.equals(newLocation, LOCATION)) LOCATION = newLocation;
+    }
+    private static String LOCATION;
 
     public static final Logger LOGGER = LoggerFactory.getLogger(ModID);
 
@@ -26,6 +31,7 @@ public class SkyblockCore implements ClientModInitializer {
         });
         LeaveSkyblockCallback.EVENT.register(() -> {
             ON_SKYBLOCK = false;
+            LOCATION = null;
             return ActionResult.PASS;
         });
     }
--- src/main/java/io/github/skyblockcore/mixin/PlayNetworkHandlerMixin.java
+++ src/main/java/io/github/skyblockcore/mixin/PlayNetworkHandlerMixin.java
@@ -5,26 +5,33 @@ import io.github.skyblockcore.SkyblockCore;
 import io.github.skyblockcore.event.JoinSkyblockCallback;
 import io.github.skyblockcore.event.LeaveSkyblockCallback;
 import net.minecraft.client.network.ClientPlayNetworkHandler;
+import net.minecraft.client.world.ClientWorld;
 import net.minecraft.network.packet.s2c.play.DisconnectS2CPacket;
 import net.minecraft.network.packet.s2c.play.ScoreboardObjectiveUpdateS2CPacket;
+import net.minecraft.scoreboard.Scoreboard;
+import net.minecraft.scoreboard.ScoreboardObjective;
+import net.minecraft.scoreboard.ScoreboardPlayerScore;
+import net.minecraft.scoreboard.Team;
 import net.minecraft.text.Text;
 import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.Shadow;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 
+import java.util.Collection;
 import java.util.Set;
 
 @Mixin(ClientPlayNetworkHandler.class)
 public class PlayNetworkHandlerMixin {
 
+    @Shadow private ClientWorld world;
     private static final Set<String> SKYBLOCK_TITLES = Sets.newHashSet("SKYBLOCK", "\u7A7A\u5C9B\u751F\u5B58", "\u7A7A\u5CF6\u751F\u5B58");
 
     @Inject(method = "onScoreboardObjectiveUpdate", at = @At("TAIL"))
     void onScoreboardDisplay(ScoreboardObjectiveUpdateS2CPacket packet, CallbackInfo ci) {
         if (SkyblockCore.isOnSkyblock()) return;
         String header = packet.getDisplayName().getString();
-        System.out.println(header);
         for (String TITLE : SKYBLOCK_TITLES) {
             if (header.contains(TITLE)) JoinSkyblockCallback.EVENT.invoker().interact();
         }
@@ -40,6 +47,23 @@ public class PlayNetworkHandlerMixin {
     void onDisconnect(Text reason, CallbackInfo ci) {
         if (!SkyblockCore.isOnSkyblock()) return;
         LeaveSkyblockCallback.EVENT.invoker().interact();
+    }
+
+    @Inject(method = "onScoreboardObjectiveUpdate", at = @At("TAIL"))
+    void onScoreboardObjective(ScoreboardObjectiveUpdateS2CPacket packet, CallbackInfo ci) {
+        if (!SkyblockCore.isOnSkyblock()) return;
+        Scoreboard scoreboard = this.world.getScoreboard();
+        ScoreboardObjective objective = scoreboard.getObjectiveForSlot(Scoreboard.SIDEBAR_DISPLAY_SLOT_ID);
+        if (objective == null) return;
+
+        Collection<ScoreboardPlayerScore> scores = scoreboard.getAllPlayerScores(objective);
+        for (ScoreboardPlayerScore score : scores) {
+            String line = Team.decorateName(scoreboard.getPlayerTeam(score.getPlayerName()), Text.literal(score.getPlayerName())).getString();
+            if (line.contains("\u23E3")) {
+                String split = line.split("\u23E3 ")[1];
+                SkyblockCore.setLocation(split.substring(0, split.length()-2));
+            }
+        }
     }
 
 }
