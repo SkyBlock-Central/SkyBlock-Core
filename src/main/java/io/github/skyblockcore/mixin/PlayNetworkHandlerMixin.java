fd50392d927fc93a40e42059b48bdcebc3b95649
Axle <bloodymountain@outlook.com>
2023-05-20 16:16:56 -0400

    Changed the way PlayerNetworkHandlerMixin.java gets the scoreboard data (We now use skytils's method I think)
    I coded this in a few moments but it built so like im sure it works (I did not check skytils code as I am not a skidder like DuckySoLucky)

--- src/main/java/io/github/skyblockcore/mixin/PlayNetworkHandlerMixin.java
+++ src/main/java/io/github/skyblockcore/mixin/PlayNetworkHandlerMixin.java
@@ -8,6 +8,7 @@ import io.github.skyblockcore.event.LocationChangedCallback;
 import io.github.skyblockcore.util.TextUtils;
 import net.minecraft.client.network.ClientPlayNetworkHandler;
 import net.minecraft.client.world.ClientWorld;
+import net.minecraft.network.packet.s2c.play.ScoreboardDisplayS2CPacket;
 import net.minecraft.network.packet.s2c.play.ScoreboardObjectiveUpdateS2CPacket;
 import net.minecraft.scoreboard.Scoreboard;
 import net.minecraft.scoreboard.ScoreboardObjective;
@@ -25,7 +26,6 @@ import java.util.Set;
 
 @Mixin(ClientPlayNetworkHandler.class)
 public class PlayNetworkHandlerMixin {
-
     @Shadow private ClientWorld world;
     private static final Set<String> SKYBLOCK_TITLES = Sets.newHashSet("SKYBLOCK", "\u7A7A\u5C9B\u751F\u5B58", "\u7A7A\u5CF6\u751F\u5B58");
 
@@ -45,11 +45,12 @@ public class PlayNetworkHandlerMixin {
         if (onSkyblock) LeaveSkyblockCallback.EVENT.invoker().interact();
     }
 
-    @Inject(method = "onScoreboardObjectiveUpdate", at = @At("TAIL"))
-    void onScoreboardObjective(ScoreboardObjectiveUpdateS2CPacket packet, CallbackInfo ci) {
+    @Inject(method = "onScoreboardDisplay", at = @At("TAIL"))
+    void onScoreboardDisplay(ScoreboardDisplayS2CPacket packet, CallbackInfo ci) {
         if (!SkyblockCore.isOnSkyblock()) return;
+
         Scoreboard scoreboard = this.world.getScoreboard();
-        ScoreboardObjective objective = scoreboard.getObjectiveForSlot(Scoreboard.SIDEBAR_DISPLAY_SLOT_ID);
+        ScoreboardObjective objective = scoreboard.getNullableObjective(packet.getName());
         if (objective == null) return;
 
         Collection<ScoreboardPlayerScore> scores = scoreboard.getAllPlayerScores(objective);
@@ -63,5 +64,4 @@ public class PlayNetworkHandlerMixin {
             }
         }
     }
-
 }
