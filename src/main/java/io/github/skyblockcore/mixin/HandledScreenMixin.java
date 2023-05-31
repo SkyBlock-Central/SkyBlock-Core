b938232d31f3a7fbf69159df19f07db5116d2c23
Kyllian <kylleke@telenet.be>
2023-05-31 13:02:47 +0200

    item lore hover text
    Adds the lore of the item as hover text on the item's name in the nbt copy text, because why not.

--- src/main/java/io/github/skyblockcore/mixin/HandledScreenMixin.java
+++ src/main/java/io/github/skyblockcore/mixin/HandledScreenMixin.java
@@ -5,9 +5,12 @@ import io.github.skyblockcore.command.SkyblockCoreCommand;
 import io.github.skyblockcore.event.ConfigManager;
 import net.minecraft.client.MinecraftClient;
 import net.minecraft.client.gui.screen.ingame.HandledScreen;
+import net.minecraft.client.item.TooltipContext;
 import net.minecraft.item.ItemStack;
 import net.minecraft.screen.ScreenHandler;
 import net.minecraft.screen.slot.Slot;
+import net.minecraft.text.HoverEvent;
+import net.minecraft.text.MutableText;
 import net.minecraft.text.Text;
 import net.minecraft.util.Formatting;
 import org.jetbrains.annotations.Nullable;
@@ -18,6 +21,8 @@ import org.spongepowered.asm.mixin.Shadow;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
+
+import java.util.List;
 
 import static io.github.skyblockcore.SkyblockCore.TITLE;
 
@@ -40,7 +45,11 @@ public class HandledScreenMixin {
         if (ConfigManager.getConfig() != null && ConfigManager.getConfig().isDev()) SkyblockCore.LOGGER.info(TITLE + " [Dev NBT] > " + itemNBT);
         MinecraftClient.getInstance().keyboard.setClipboard(itemNBT);
         if (MinecraftClient.getInstance().player == null) return;
-        MinecraftClient.getInstance().player.sendMessage(Text.literal(TITLE + " NBT Data has been copied for: ").formatted(Formatting.WHITE).append(itemToCopyNBT.getName()), false);
+        MutableText tooltip = Text.literal("");
+        List<Text> loreLines = itemToCopyNBT.getTooltip(MinecraftClient.getInstance().player, TooltipContext.BASIC);
+        for (int i = 0; i<loreLines.size(); i++)
+            tooltip.append((i==0) ? "" : "\n").append(loreLines.get(i));
+        MinecraftClient.getInstance().player.sendMessage(Text.literal(TITLE + " NBT Data has been copied for: ").formatted(Formatting.WHITE).append(((MutableText) itemToCopyNBT.getName()).styled(s -> s.withHoverEvent(new HoverEvent(HoverEvent.Action.SHOW_TEXT, tooltip)))), false);
     }
 
 }
