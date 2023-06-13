9346bd82f89954ad77785b4667db5d33b83276d8
Zickles <76439587+Zickles@users.noreply.github.com>
2023-06-13 19:00:36 -0400

    add try catch

--- src/main/java/io/github/skyblockcore/util/APIUtils.java
+++ src/main/java/io/github/skyblockcore/util/APIUtils.java
@@ -4,7 +4,6 @@ import com.google.gson.JsonElement;
 import com.google.gson.JsonParser;
 
 import java.io.BufferedReader;
-import java.io.IOException;
 import java.io.InputStreamReader;
 import java.net.URL;
 import java.net.URLConnection;
@@ -12,22 +11,30 @@ import java.net.URLConnection;
 public class APIUtils {
 
     private static JsonElement PlayerInfo;
-    public static String getUsername(String uuid) throws IOException {
-        URL userinfo = new URL("https://sessionserver.mojang.com/session/minecraft/profile/" + uuid);
-        URLConnection urlConnect = userinfo.openConnection();
-        BufferedReader in = new BufferedReader(new InputStreamReader(urlConnect.getInputStream()));
-        PlayerInfo = JsonParser.parseReader(in);
+    public static String getUsername(String uuid) {
+        try {
+            URL userinfo = new URL("https://sessionserver.mojang.com/session/minecraft/profile/" + uuid);
+            URLConnection urlConnect = userinfo.openConnection();
+            BufferedReader in = new BufferedReader(new InputStreamReader(urlConnect.getInputStream()));
+            PlayerInfo = JsonParser.parseReader(in);
 
-        in.close();
+            in.close();
+        } catch(Exception e) {
+            System.out.println(e);
+        }
         return PlayerInfo.getAsJsonObject().get("name").getAsString();
     }
-    public static String getUUID(String username) throws IOException {
-        URL userinfo = new URL("https://api.mojang.com/users/profiles/minecraft/" + username);
-        URLConnection urlConnect = userinfo.openConnection();
-        BufferedReader in = new BufferedReader(new InputStreamReader(urlConnect.getInputStream()));
-        PlayerInfo = JsonParser.parseReader(in);
+    public static String getUUID(String username) {
+        try {
+            URL userinfo = new URL("https://api.mojang.com/users/profiles/minecraft/" + username);
+            URLConnection urlConnect = userinfo.openConnection();
+            BufferedReader in = new BufferedReader(new InputStreamReader(urlConnect.getInputStream()));
+            PlayerInfo = JsonParser.parseReader(in);
 
-        in.close();
+            in.close();
+        } catch (Exception e) {
+            System.out.println(e);
+        }
         return PlayerInfo.getAsJsonObject().get("id").getAsString();
     }
 }
