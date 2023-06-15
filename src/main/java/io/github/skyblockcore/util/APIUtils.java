6c679572868598150317110713150272db30645c
Zickles <76439587+Zickles@users.noreply.github.com>
2023-06-15 17:49:00 -0400

    fixes my sleepy 5 am code

--- src/main/java/io/github/skyblockcore/util/APIUtils.java
+++ src/main/java/io/github/skyblockcore/util/APIUtils.java
@@ -1,71 +1,65 @@
 package io.github.skyblockcore.util;
 
+import com.google.common.collect.BiMap;
+import com.google.common.collect.HashBiMap;
 import com.google.gson.JsonElement;
 import com.google.gson.JsonParser;
 
 import java.io.BufferedReader;
+import java.io.IOException;
 import java.io.InputStreamReader;
 import java.net.URL;
 import java.net.URLConnection;
-import java.util.HashMap;
-import java.util.Map;
-import java.util.Objects;
+
+
+import static io.github.skyblockcore.SkyBlockCore.LOGGER;
 
 public class APIUtils {
 
-    private static JsonElement PlayerInfo;
-    private static String Player;
-
-    private static HashMap<String, String> PlayerData = new HashMap<String, String>();
+    private static BiMap<String, String> CachedData = HashBiMap.create();
     public static String getUsername(String uuid) {
-        if (PlayerData.containsValue(uuid)) {
-            for(Map.Entry<String, String> entry : PlayerData.entrySet()){
-                if(Objects.equals(entry.getValue(), uuid)) {
-                    Player = entry.getKey();
-                }
-            }
+        JsonElement PlayerData = null;
+        if (CachedData.containsValue(uuid)) {
             System.out.println("Using cached player data...");
-            return Player;
+            return CachedData.inverse().get(uuid);
         }
-        else {
-            try {
-                URL userinfo = new URL("https://sessionserver.mojang.com/session/minecraft/profile/" + uuid);
-                URLConnection urlConnect = userinfo.openConnection();
-                BufferedReader in = new BufferedReader(new InputStreamReader(urlConnect.getInputStream()));
-                PlayerInfo = JsonParser.parseReader(in);
+        try {
+            URL userinfo = new URL("https://sessionserver.mojang.com/session/minecraft/profile/" + uuid);
+            URLConnection urlConnect = userinfo.openConnection();
 
-                PlayerData.put(PlayerInfo.getAsJsonObject().get("name").getAsString(), PlayerInfo.getAsJsonObject().get("id").getAsString());
+            BufferedReader in = new BufferedReader(new InputStreamReader(urlConnect.getInputStream()));
 
-                in.close();
-            } catch(Exception e) {
-                System.out.println(e);
-            }
-            return PlayerInfo.getAsJsonObject().get("name").getAsString();
+            PlayerData = JsonParser.parseReader(in);
+
+        } catch(IOException e) {
+            LOGGER.info(String.valueOf(e));
         }
+
+        CachedData.put(PlayerData.getAsJsonObject().get("name").getAsString(), PlayerData.getAsJsonObject().get("id").getAsString());
+
+        return PlayerData.getAsJsonObject().get("name").getAsString();
     }
+
     public static String getUUID(String username) {
-        if (PlayerData.containsKey(username)) {
-            for(Map.Entry<String, String> entry : PlayerData.entrySet()){
-                if(Objects.equals(entry.getKey(), username)) {
-                    Player = entry.getValue();
-                }
-            }
+        JsonElement PlayerData = null;
+        if (CachedData.containsKey(username)) {
             System.out.println("Using cached player data...");
-            return Player;
-        } else {
-              try {
-                  URL userinfo = new URL("https://api.mojang.com/users/profiles/minecraft/" + username);
-                  URLConnection urlConnect = userinfo.openConnection();
-                  BufferedReader in = new BufferedReader(new InputStreamReader(urlConnect.getInputStream()));
-                  PlayerInfo = JsonParser.parseReader(in);
-
-                  PlayerData.put(PlayerInfo.getAsJsonObject().get("name").getAsString(), PlayerInfo.getAsJsonObject().get("id").getAsString());
-
-                  in.close();
-              } catch (Exception e) {
-                  System.out.println(e);
-              }
-            return PlayerInfo.getAsJsonObject().get("id").getAsString();
+            return CachedData.get(username);
         }
+        try {
+            URL userinfo = new URL("https://api.mojang.com/users/profiles/minecraft/" + username);
+            URLConnection urlConnect = userinfo.openConnection();
+
+            BufferedReader in = new BufferedReader(new InputStreamReader(urlConnect.getInputStream()));
+
+            PlayerData = JsonParser.parseReader(in);
+
+        } catch (IOException e) {
+            LOGGER.info(String.valueOf(e));
+        }
+
+        CachedData.put(PlayerData.getAsJsonObject().get("name").getAsString(), PlayerData.getAsJsonObject().get("id").getAsString());
+
+        return PlayerData.getAsJsonObject().get("id").getAsString();
     }
 }
