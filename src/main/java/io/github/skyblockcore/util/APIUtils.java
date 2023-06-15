d6e87fa9937a2e5ec6d5d657c37f3b50c8cc8d2c
Zickles <76439587+Zickles@users.noreply.github.com>
2023-06-15 18:50:34 -0400

    remove unneeded import

--- src/main/java/io/github/skyblockcore/util/APIUtils.java
+++ src/main/java/io/github/skyblockcore/util/APIUtils.java
@@ -8,7 +8,6 @@ import com.google.gson.JsonParser;
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.net.MalformedURLException;
 import java.net.URL;
 import java.net.URLConnection;
 
